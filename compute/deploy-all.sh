01aba878901d42be1d4fb47eb2232a48ac81500c
life1347 <life1347@gmail.com>
2013-12-01 04:06:37 +0800

    New

--- compute/deploy-all.sh
+++ compute/deploy-all.sh
@@ -2,34 +2,6 @@
 
 ./configure-system.sh
 
-./install-mysql.sh
-
-./initialize-mysql.sh
-
-./install-rabbitmq.sh
-
-./install-keystone.sh
-
-./configure-keystone.sh
-
-./initialize-keystone.sh
-
-./install-glance.sh
-
-./configure-glance.sh
-
-./initialize-glance.sh
-
-./install-cinder.sh
-
-./configure-cinder.sh
-
-./install-dashboard.sh
-
-./install-heat.sh
-
-./configure-heat.sh
-
 ./install-neutron.sh
 
 ./configure-neutron.sh
