7f56cead9f7b77056056a12bef7c54784b87a01d
life1347 <life1347@gmail.com>
2013-12-11 22:21:30 +0800

    add arp replysetting
    net.ipv4.conf.all.arp_ignore=1
    net.ipv4.conf.all.arp_announce=2

--- configure-system.sh
+++ configure-system.sh
@@ -44,6 +44,8 @@ net.ipv4.ip_forward = 1
 net.ipv4.conf.all.forwarding = 1
 net.ipv4.conf.all.rp_filter = 0
 net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.all.arp_ignore=1
+net.ipv4.conf.all.arp_announce=2
 EOF
 
 echo "$HOST_IP $CONTROL_HOST_NAME" >> /etc/hosts
