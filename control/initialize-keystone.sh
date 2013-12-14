16457c25a2a9b6a8d1e34c3277527940dbc71506
life1347 <life1347@gmail.com>
2013-12-15 05:30:12 +0800

    add heat/ceilometer user&endpoints

--- control/initialize-keystone.sh
+++ control/initialize-keystone.sh
@@ -2,27 +2,6 @@
 
 source ../openrc
 
-# Modify these variables as needed
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-swordfish}
-SERVICE_PASSWORD=${SERVICE_PASSWORD:-$ADMIN_PASSWORD}
-DEMO_PASSWORD=${DEMO_PASSWORD:-$ADMIN_PASSWORD}
-
-export OS_SERVICE_TOKEN="swordfish"
-export OS_SERVICE_ENDPOINT="http://$HOST_IP:35357/v2.0"
-
-SERVICE_TENANT_NAME=${SERVICE_TENANT_NAME:-service}
-
-#
-MYSQL_USER=keystone
-MYSQL_DATABASE=keystone
-MYSQL_HOST=localhost
-MYSQL_PASSWORD=swordfish
-
-#
-KEYSTONE_REGION=RegionOne
-KEYSTONE_HOST=$HOST_IP
-
-
 # Shortcut function to get a newly generated ID
 function get_field() {
     while read data; do
@@ -83,6 +62,18 @@ CINDER_USER=$(keystone user-create \
   --tenant-id $SERVICE_TENANT \
   --email cinder@domain.com | grep " id " | get_field 2)
 
+HEAT_USER=$(keystone user-create \
+  --name heat \
+  --pass "$SERVICE_PASSWORD" \
+  --tenant-id $SERVICE_TENANT \
+  --email heat@domain.com | grep " id " | get_field 2)
+
+CEILOMETER_USER=$(keystone user-create \
+  --name ceilometer \
+  --pass "$SERVICE_PASSWORD" \
+  --tenant-id $SERVICE_TENANT \
+  --email ceilometer@domain.com | grep " id " | get_field 2)
+
 # Roles
 ADMIN_ROLE=$(keystone role-create --name=admin | grep " id " | get_field 2)
 MEMBER_ROLE=$(keystone role-create --name=Member | grep " id " | get_field 2)
@@ -93,8 +84,9 @@ keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $NOVA_USER --role-i
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $GLANCE_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $QUANTUM_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $CINDER_USER --role-id $ADMIN_ROLE
+keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $HEAT_USER --role-id $ADMIN_ROLE
+keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $CEILOMETER_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $DEMO_TENANT --user-id $DEMO_USER --role-id $MEMBER_ROLE
-
 
 # Create services
 COMPUTE_SERVICE=$(keystone service-create \
@@ -132,54 +124,66 @@ HEAT_SERVICE=$(keystone service-create \
   --type orchestration \
   --description 'HEAT Orchestration API' | grep " id " | get_field 2)
 
+CEILOMETER_SERVICE=$(keystone service-create \
+  --name ceilometer \
+  --type metering \
+  --description 'Ceilometer Telemetry Service' | grep " id " | get_field 2)
+
 
 # Create endpoints
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $COMPUTE_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s'
+  --publicurl 'http://'"$HOST_IP"':8774/v2/$(tenant_id)s' \
+  --adminurl 'http://'"$HOST_IP"':8774/v2/$(tenant_id)s' \
+  --internalurl 'http://'"$HOST_IP"':8774/v2/$(tenant_id)s'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $VOLUME_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s'
+  --publicurl 'http://'"$HOST_IP"':8776/v1/$(tenant_id)s' \
+  --adminurl 'http://'"$HOST_IP"':8776/v1/$(tenant_id)s' \
+  --internalurl 'http://'"$HOST_IP"':8776/v1/$(tenant_id)s'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $IMAGE_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':9292' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':9292' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':9292'
+  --publicurl 'http://'"$HOST_IP"':9292' \
+  --adminurl 'http://'"$HOST_IP"':9292' \
+  --internalurl 'http://'"$HOST_IP"':9292'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $IDENTITY_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':5000/v2.0' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':35357/v2.0' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':5000/v2.0'
+  --publicurl 'http://'"$HOST_IP"':5000/v2.0' \
+  --adminurl 'http://'"$HOST_IP"':35357/v2.0' \
+  --internalurl 'http://'"$HOST_IP"':5000/v2.0'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $EC2_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':8773/services/Admin' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud'
+  --publicurl 'http://'"$HOST_IP"':8773/services/Cloud' \
+  --adminurl 'http://'"$HOST_IP"':8773/services/Admin' \
+  --internalurl 'http://'"$HOST_IP"':8773/services/Cloud'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $NETWORK_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':9696/' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':9696/' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':9696/'
+  --publicurl 'http://'"$HOST_IP"':9696/' \
+  --adminurl 'http://'"$HOST_IP"':9696/' \
+  --internalurl 'http://'"$HOST_IP"':9696/'
 
 keystone endpoint-create \
   --region $KEYSTONE_REGION \
   --service-id $HEAT_SERVICE \
-  --publicurl 'http://'"$KEYSTONE_HOST"':8004/v1/%(tenant_id)s' \
-  --adminurl 'http://'"$KEYSTONE_HOST"':8004/v1/%(tenant_id)s' \
-  --internalurl 'http://'"$KEYSTONE_HOST"':8004/v1/%(tenant_id)s'
+  --publicurl 'http://'"$HOST_IP"':8004/v1/%(tenant_id)s' \
+  --adminurl 'http://'"$HOST_IP"':8004/v1/%(tenant_id)s' \
+  --internalurl 'http://'"$HOST_IP"':8004/v1/%(tenant_id)s'
+
+keystone endpoint-create \
+  --region $KEYSTONE_REGION \
+  --service-id $CEILOMETER_SERVICE \
+  --publicurl 'http://'"$HOST_IP"':8777/' \
+  --adminurl 'http://'"$HOST_IP"':8777/' \
+  --internalurl 'http://'"$HOST_IP"':8777/'
 
